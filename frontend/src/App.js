afa8f7494e0d2e9e3ac60fa6adad008f1c59567b
aamir socatali <hk61900@gmail.com>
2024-06-12 20:32:10 +0530

    completed ui of login, signup page and set up of routing

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,28 @@
 import './App.css';
 import Home from './pages/Home';
+import LoginPage from './pages/LoginPage';
+import SignupPage from './pages/SignupPage';
+import {RouterProvider, createBrowserRouter} from 'react-router-dom'
 
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path:"/",
+      element:(<Home/>)
+    },
+    {
+      path:"/signup",
+      element:(<SignupPage/>)
+    },
+    {
+      path:"/login",
+      element:(<LoginPage/>)
+    }
+  ])
   return (
     <div className="App">
-      <Home/>
+      <RouterProvider router={router}/>
     </div>
   );
 }
